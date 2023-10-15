53445db6d749b28a5e97d3b411fc20f06f73621c
Rishita Dhalbisoi <rishita.dhalbisoi@gmail.com>
2023-10-14 21:49:23 -0400

    basic modal working

--- script.js
+++ script.js
@@ -15,9 +15,73 @@ function displayImages() {
         const img = document.createElement('img');
         img.src = imageFolderPath + imageName;
         img.alt = imageName;
+        img.className = 'modal-image';
         imageGrid.appendChild(img);
     });
 }
 
 // Call the function to display the images
 displayImages();
+
+// Get all image elements (both existing and dynamically generated)
+var images = document.querySelectorAll('.modal-image');
+
+images.forEach(function(image, index) {
+    // Create a modal for each image
+    var modal = document.createElement('div');
+    modal.className = 'modal';
+    modal.id = 'modal' + (index + 1);
+
+    // Create modal content
+    var modalContent = document.createElement('img');
+    modalContent.className = 'modal-content';
+    modalContent.id = 'modalImage' + (index + 1);
+
+    // Create a close button
+    var closeButton = document.createElement('span');
+    closeButton.className = 'close';
+    closeButton.id = 'closeModal' + (index + 1);
+    closeButton.innerHTML = '&times;';
+
+    /*
+    // Create a container for the image and text
+    var image2Container = document.createElement('div');
+    image2Container.className = 'image2-container';
+    image2Container.appendChild(image);
+    
+    var textContainer = document.createElement('div');
+    textContainer.className = 'text-container';
+    textContainer.innerHTML = `
+        <p>This is some text about Image ${index}.</p>
+        <!-- You can add more text or customize as needed -->`;
+    */
+
+    // Append modal content and close button to the modal
+    modal.appendChild(closeButton);
+    //modalContent.appendChild(image2Container);
+    //modalContent.appendChild(textContainer);
+    modal.appendChild(modalContent);
+
+    // Append the modal to the body
+    document.body.appendChild(modal);
+
+    // Add click event listener to open the modal
+    image.addEventListener('click', function() {
+        openModal(index + 1);
+    });
+});
+
+// Function to open the modal with the given index
+function openModal(index) {
+    var modal = document.getElementById('modal' + index);
+    var modalImage = document.getElementById('modalImage' + index);
+
+    modal.style.display = 'block';
+    modalImage.src = images[index - 1].src;
+
+    // Close the modal when the close button is clicked
+    var closeButton = document.getElementById('closeModal' + index);
+    closeButton.addEventListener('click', function() {
+        modal.style.display = 'none';
+    });
+}
