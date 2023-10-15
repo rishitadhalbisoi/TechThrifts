811a41d40ecae333bc53a2817faea2f51fc69607
Japleen <1kjapleen@gmail.com>
2023-10-15 06:58:20 -0400

    changes

--- mapmarker.js
+++ mapmarker.js
@@ -33,8 +33,12 @@ function initMap() {
     const detailWindow = new google.maps.InfoWindow({
       content:property.content
     });
+    marker.addListener("mouseover", () =>{
+      detailWindow.open(map, marker);
+    })
   }
 
+  addMarker({location: {lat: 33.7756, lng: -84.3963}, content: `<h2>Tech Green: Every Wednesday from 10am to 2pm</h2>`});
+  addMarker({location: {lat: 33.77974245201469, lng: -84.40470499398494}, content: `<h2>West Village: October 22nd from 3pm to 5pm</h2>`});
+  addMarker({location: {lat: 33.77728776563519, lng: -84.39582582126907}, content: `<h2>Klaus: December 8th from 1pm to 4pm</h2>`});
 }
-
-
